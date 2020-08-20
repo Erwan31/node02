85962464379f639292ccbc702b882e8d639d7e7a
Erwan31 <spilmonterwan@gmail.com>
2020-08-20 18:15:49 +0100

    Update

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
+
+const url = 'mongodb://localhost:27017'
+
+
+app.get('/api/users',(req,res)=>{
+    MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+        if(err){
+            console.log('could not connect to DB');
+        }
+        console.log('connected to test DB');
+        client.close();
+    })
+})
+
 
 const port = process.env.PORT || 3001;
 
 app.listen(port);
+
