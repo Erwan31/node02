eadd8d2735a6c5adb1c16a875296d730fd8bb882
Erwan31 <spilmonterwan@gmail.com>
2020-08-21 12:00:50 +0100

    Post & Get with Mongoose

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,42 @@ import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    cars: [],
+  }
+
   componentDidMount(){
-    axios.get('/api/users').then(response => {
+    axios.get('/api/getcars').then(response => {
+      this.setState({cars: response.data});
       console.log(response.data)
+    })
+  }
+
+  onCarSubmit = () => {
+    axios.post('/api/addcar', {
+      brand: "Ford",
+      model: "Fiesta",
+      year: 2016,
+      avail: true,
+    }).then( response => {
+      console.log(response.data);
     })
   }
 
   render(){
     return (
-      <>
-        <h1>Hello</h1>
-      </>
+      <div className="App">
+        <h1>Add Car</h1>
+        <button onClick={() => this.onCarSubmit()}>
+          Add Car to db
+        </button>
+        <br/>
+        {
+          this.state.cars.map( (car, i) => (
+            <div key={i}>- {car.model}</div>
+          ))
+        }
+      </div>
     );
   }
 
--- server.js
+++ server.js
@@ -1,10 +1,84 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+//const { MongoClient } = require('mongodb');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to DB
+mongoose.connect('mongodb://localhost:27017/App',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+
+// Create Mongoose Schema + Model
+const carSchema = mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    avail: Boolean,
+});
+
+const Car = mongoose.model('Car', carSchema);
+////////////////////////////////////////////////
+
+app.post('/api/addcar', (req,res) => {
+    
+    const addCar = new Car({
+        brand: req.body.brand,
+        model: req.body.model,
+        year: req.body.year,
+        avail: req.body.avail,
+    })
+    
+    
+    addCar.save((err, doc) => {
+        if(err) return console.log(err);
+        console.log(doc);
+        res.sendStatus(200);
+    })
+});
+
+
+
+app.get('/api/getcars', (req, res) => {
+    Car.find( {}, (err, doc) => {
+        if(err) return console.log(err);
+        res.json(doc);
+    })
+})
+
+/*
 const url = 'mongodb://localhost:27017'
 
+const cars = [
+    {model:"chevy", year:2017},
+    {model: "nissan", year: 2000},
+];
 
+/*
+app.get('/api/users',(req,res)=>{
+    MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+        if(err){
+            console.log('COuld not connect to DB...');
+        }
+        console.log('Could connect to DB !!');
+        client.db('Cars').collection('items').insert({
+            cars
+        }, (err, res) => {
+            if(err){
+                return console.log(`ERROR: ${err}`)
+            }
+            console.log(res.ops[0]._id.getTimestamp());
+            client.close();
+        });
+    });
+})
+
+/*
 app.get('/api/users',(req,res)=>{
     MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
         if(err){
@@ -23,7 +97,7 @@ app.get('/api/users',(req,res)=>{
         });
     });
 })
-
+*/
 
 const port = process.env.PORT || 3001;
 
